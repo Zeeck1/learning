1e0b738b47e6d9cf8594e0781c24c924af515e64
Zeeck1 <168504070+Zeeck1@users.noreply.github.com>
2024-05-10 10:02:05 +0700

    admin.py

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 admin.site.register(Course)
 admin.site.register(Lession)
 admin.site.register(UserCourseLink)
 admin.site.register(QuizQuestion)
 admin.site.register(QuizResult)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('date_joined',)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
